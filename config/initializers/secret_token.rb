08a18d56372556947f9a5f2e8b2dda95e903c3e1
Karsten Silkenbäumer <ksi@kluks.de>
2013-08-13 00:23:40 +0200

    Initial commit
    Commands:
      rails new demo -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '5f026db4457938e2c7df997cf295a91eeac6c0dc4555281b76f7d78a025d2ad174d1604e4e5b31b5ba53881beb38f7232891ef3830a3fe0e59188641c256a415'
